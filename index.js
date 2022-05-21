8ca04439228abfd33db42835c00a81a564bdb2cc
LUSIFE <62038653+LU-SIFE@users.noreply.github.com>
2022-05-21 13:39:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var mouse, raycaster;
