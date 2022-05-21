dc58b2bbcdb51dc424149897a6f85a1a07f99379
LUSIFE <62038653+LU-SIFE@users.noreply.github.com>
2022-05-21 13:57:42 -0600

    import module fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
+import * as THREE from "js/three.js";
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var mouse, raycaster;
