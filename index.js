669468061dc375cd26d0f0dce33ad54b4a8e51cd
LUSIFE <62038653+LU-SIFE@users.noreply.github.com>
2022-05-21 14:14:41 -0600

    removed OrbitControls.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import * as THREE from 'js/three.js';
-import { OrbitControls } from 'js/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
