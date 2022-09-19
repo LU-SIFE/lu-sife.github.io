8aa7a418d5ee3619cc84b1fc1bd171f0ecec306f
LU SIFE <michaelleix@outlook.com>
2022-09-19 08:06:48 -0600

    new import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.skypack.dev/three';
+import * as THREE from 'three';
 //event listeners
 window.addEventListener('click', onMouseClick, false);
 window.addEventListener('mousemove', onMouseMove, false);
