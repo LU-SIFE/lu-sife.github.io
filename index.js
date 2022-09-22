2c5f34e96217b629b66195eb9b5b43b5930ead7f
LU SIFE <michaelleix@outlook.com>
2022-09-21 22:17:21 -0600

    added page buttons :D

--- index.js
+++ index.js
@@ -449,28 +449,6 @@ function shuffle(array) {
 function praise_the_code() {
 	alert("Woohoo! secret lol");
 }
-// Get the button:
-let mybutton = document.getElementById("return_button");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
-    mybutton.style.opacity = 1;
-  } else {
-    mybutton.style.opacity = 0;
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function return_to_top() {
-	if (mybutton.style.opacity == 1) {
-  	document.body.scrollTop = 0; // For Safari
-  	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-	}
-}
-
 
 //main animation function
 //DO NOT INCLUDE RETURN;
