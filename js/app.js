33fa33814191bf93c277a2f7198b84d49ebfc8fb
LU SIFE <michaelleix@outlook.com>
2022-10-04 09:53:26 -0600

    minified three.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
-
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
-*/
-
-/* Otherwise just put the config content (json): */
-
 particlesJS('particles-js',
 {
   "particles": {
@@ -122,5 +109,4 @@ particlesJS('particles-js',
   },
   "retina_detect": true
 }
-
 );
