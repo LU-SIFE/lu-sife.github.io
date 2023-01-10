549aa49526c7612381d4acf963a8302f7975ffab
LU SIFE <michaelleix@outlook.com>
2023-01-10 10:45:02 -0700

    changed some links

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ window.addEventListener('mousemove', onMouseMove, false);
 window.addEventListener('resize', onWindowResize, false);
 
 var data_array = [
-	["Web Dev Ticketing", "https://lu-sife.github.io/ticketing", "#6D00CF"],
+	["(WIP) Untitled Game", "https://lu-sife.github.io/untitled-game", "#6D00CF"],
 	["Puzzle Crawler", "https://lu-sife.github.io/Puzzle-Crawler/", "#C71266"],
-	["LEVELED (WIP)", "https://lu-sife.github.io/LEVELED/", "#F25050"]
+	["(WIP) LEVELED", "https://lu-sife.github.io/LEVELED/", "#F25050"]
 ];
 
 //scene initialization
