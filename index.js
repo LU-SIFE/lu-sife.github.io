ac60d2e8c7bfca486a5942c92e52608ce0bddd81
Lu-Sife <itslusife@gmail.com>
2024-08-17 17:58:38 -0600

    redesign!

--- index.js
+++ index.js
@@ -1,83 +1,47 @@
-// moves cursor to correct position
-document.onmousemove = function(event) {
-  mouse.x = event.clientX - window.innerWidth / 2;
-  mouse.y = event.clientY - window.innerHeight / 2;
-  document.getElementById('cursor').style.left = event.clientX + 'px';
-  document.getElementById('cursor').style.top = event.clientY + 'px';
-};
+anime({
+	targets: '.b1',
+	scaleX: ["1.5", "1", "1.1", "1"],
+	scaleY: ["0.8", "0.8", "1", "1.1"],
+	rotate: ["-15deg", "10deg"],
 
-//changes cursor state onhover
-function ch(state) {
-  if (state === true) {
-    document.getElementById("cursor").classList.remove("cursor_shrink");
-    document.getElementById("cursor").classList.add("cursor_expand");
-  } else {
-    document.getElementById("cursor").classList.remove("cursor_expand");
-    document.getElementById("cursor").classList.add("cursor_shrink");
-  }
-}
-
-var elems = document.getElementsByClassName("letter");
-
-//refreshes the time
-function refreshTime() {
-  var dateString = new Date().toLocaleString("en-US", {
-    timeZone: "America/Denver", 
-    hour12: false,
-  });
-
-  var formattedString = dateString.replace(", ", " - ");
-  document.getElementById("time").innerHTML = formattedString + " MDT <3";
-}
-
-const scrollPercent = () => {
-  const bodyST = document.body.scrollTop;
-  const docST = document.documentElement.scrollTop;
-  const docSH = document.documentElement.scrollHeight;
-  const docCH = document.documentElement.clientHeight;
-  return (docST + bodyST) / (docSH - docCH) * 100
-}
-
-function idle_text() {
-  anime.timeline().add({
-  targets: '.ml16 .letter',
-  translateY: ["0em", "-1.2em", "0em"], 
-  easing: "easeOutElastic(0,0.8)",
-  duration: 1000,
-  delay: (el, i) => 40 * i
+	duration: 14000,
+	delay: 500,
+	endDelay: 500,
+	loop: true,
+	direction: 'alternate',
+	easing: 'easeInOutQuad',
 });
 
-  anime.timeline().add({
-  targets: '.text2',
-  rotate: ["0deg", "1deg", "-1deg", "0deg"],
-  easing: "easeOutElastic(0,0.8)",
-  duration: 500,
-  delay: (el, i) => 250 * i + 1000
+anime({
+	targets: '.b2',
+	scaleX: ["1.5", "1.2", "1.1", "1"],
+	scaleY: ["1.8", "1.6", "1.3", "1.4"],
+	rotate: ["-15deg", "-5deg"],
+
+	duration: 15000,
+	delay: 500,
+	endDelay: 750,
+	loop: true,
+	direction: 'alternate',
+	easing: 'easeInOutQuad',
 });
-}
 
-function parallax() {
-  document.getElementById('scroll_nav').style.translate = -(mouse.x/200) + "px " + -(mouse.y/65) + "px";
-  document.getElementById('nav').style.translate = -(mouse.x/200) + "px " + -(mouse.y/65) + "px";
-  document.querySelector('.scroll').style.translate = -(mouse.x/200) + "px " + -(mouse.y/65) + "px";
-  document.querySelector('.sect1').style.translate = -(mouse.x/200) + "px " + -(mouse.y/65) + "px";
-  document.querySelector('.sect3').style.translate = -(mouse.x/200) + "px " + -(mouse.y/65) + "px";
-  document.querySelector('.text_container').style.translate = -(mouse.x/200) + "px " + -(mouse.y/65) + "px";
-  document.querySelector('#time_container').style.translate = -(mouse.x/200) + "px " + -(mouse.y/65) + "px";
-}
+var colorMode = 0; //dark
 
-var donate = false;
-var mouse = new Object();
-var bar_counter = false;
-var scroll_counter = false;
-var toggle_counter = false;
-var page_state = "none";
-
-window.onload = function() {
-  refreshTime();
-  setInterval(refreshTime, 1000);
-  setInterval(idle_text, 15000);
-  document.querySelectorAll('ul.cloud a').forEach((i) => {
-    i.style.setProperty('--size', i.dataset.weight);
-    });
+function color_mode() {
+	if (colorMode === 0) {
+		colorMode = 1;
+		anime({
+			targets: 'body',
+			backgroundColor: '#FFFFFF',
+			duration: 0
+		});
+	} else if (colorMode === 1) {
+		colorMode = 0;
+		anime({
+			targets: 'body',
+			backgroundColor: "#1F1F1F",
+			duration: 0
+		});
+	}
 }
